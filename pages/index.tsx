41a8250e7ce8f5c2604705a81f701d241785a8e0
Jello Cho <jello.cho@linethree.com>
2021-04-21 15:34:05 +0900

    pre-rendering and data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import { FC } from "react";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+type PostType = {
+  id: string;
+  date: string;
+  title: string;
+};
+
+type HomeProps = {
+  allPostsData: Array<PostType>;
+};
+
+const Home: FC<HomeProps> = ({ allPostsData }) => {
   return (
     <Layout home>
       <Head>
@@ -15,6 +36,22 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
-}
+};
+
+export default Home;
